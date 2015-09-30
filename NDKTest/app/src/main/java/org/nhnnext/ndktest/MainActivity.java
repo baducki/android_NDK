68c157268bccf5d91bcc91a28e1f7630260a574c
YUN YOUNGGI <baducki77@naver.com>
2015-09-30 23:25:24 +0900

    finished

--- NDKTest/app/src/main/java/org/nhnnext/ndktest/MainActivity.java
+++ NDKTest/app/src/main/java/org/nhnnext/ndktest/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("NDKTest");
     }
 
-    public native String getStringFromNativeEJ();
+    public native String getStringFromNativeYG();
     public native String getStringFromNativeNDK();
     public native String getAddedStringFromNative();
 
@@ -21,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TextView에 Native Method를 호출한 결과를 표시한다.
 
         TextView view1 = (TextView) findViewById(R.id.textView1);
         TextView view2 = (TextView) findViewById(R.id.textView2);
@@ -35,19 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
